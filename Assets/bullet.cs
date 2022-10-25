aaf715f8834c7ae776e357404df8f06c26e409fc
Quetip <wroussel8365@gmail.com>
2022-10-24 22:24:37 -0400

    added new sprites and script background

--- Assets/bullet.cs
+++ Assets/bullet.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class bullet : MonoBehaviour
-{
-
+{      
     public GameObject hitEffect;
-      
-
     void OnCollisionEnter2D(Collision2D collision) 
     {
-        collision.relativeVelocity
+
     }
 
 }
