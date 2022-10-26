ce6e4c6dda1b83fd9867b566a0217131515b8183
Quetip <wroussel8365@gmail.com>
2022-10-25 23:09:14 -0400

    setting upfor native tile editor

--- Assets/Scripts/background.cs
+++ Assets/Scripts/background.cs
@@ -19,10 +19,12 @@ public class background : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect))
         float dist = (cam.transform.position.x * parallaxEffect);
 
         //moves camera
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.x);
-
+        
+        
     }
 }
