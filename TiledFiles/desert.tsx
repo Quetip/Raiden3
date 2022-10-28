eb7b4e4f2752340f3edebbc96b190be2920a59bb
Quetip <wroussel8365@gmail.com>
2022-10-27 21:18:56 -0400

    added grid tilemap

---
+++ TiledFiles/desert.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="desert" tilewidth="32" tileheight="32" tilecount="70" columns="7">
+ <image source="../Assets/Tiles/desert.png" width="240" height="320"/>
+</tileset>
