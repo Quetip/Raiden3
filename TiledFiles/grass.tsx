959e113b89e27fe94d989d1ac3474507554cd953
Quetip <wroussel8365@gmail.com>
2022-10-29 21:20:13 -0400

    added tiles

---
+++ TiledFiles/grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="grass" tilewidth="64" tileheight="64" tilecount="15" columns="3">
+ <image source="../Assets/Sprites/0004.png" width="240" height="320"/>
+</tileset>
